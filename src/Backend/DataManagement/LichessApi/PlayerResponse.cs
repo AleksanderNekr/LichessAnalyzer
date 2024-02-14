550e727c91451119d6e6f0eaddbe39a3ba99dbea
aleks <aleks@email.noreply.com>
2024-02-14 18:48:09 +0500

    [Back] LichessApi (feat): add player and team response models.

---
+++ src/Backend/DataManagement/LichessApi/PlayerResponse.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Backend.DataManagement.LichessApi;
+
+public record PlayerResponse(
+    [property: JsonPropertyName("nickname")]         string                             Nickname,
+    [property: JsonPropertyName("id")]               string                             Id,
+    [property: JsonPropertyName("ratingsHistories")] IReadOnlyList<RatingHistory>       RatingsHistories,
+    [property: JsonPropertyName("gamesHistory")]     IReadOnlyList<GamesList>           GamesLists,
+    [property: JsonPropertyName("statistics")]       IReadOnlyList<Statistic>           Statistics,
+    [property: JsonPropertyName("tournaments")]      IReadOnlyList<TournamentStatistic> Tournaments,
+    [property: JsonPropertyName("teams")]            IReadOnlyList<TeamResponse>        Clubs
+);
+
+public record GamesList(
+    [property: JsonPropertyName("nicknameForWhite")] string       NicknameForWhite,
+    [property: JsonPropertyName("nicknameForBlack")] string       NicknameForBlack,
+    [property: JsonPropertyName("gameResult")]       GameResult   GameResult,
+    [property: JsonPropertyName("winnerRateChange")] int          WinnerRateChange,
+    [property: JsonPropertyName("loserChangeRate")]  int          LoserChangeRate,
+    [property: JsonPropertyName("gameType")]         PlayCategory GameType,
+    [property: JsonPropertyName("gameDate")]         DateTime     GameDate
+);
+
+public record RatingHistory(
+    [property: JsonPropertyName("category")]      PlayCategory Category,
+    [property: JsonPropertyName("rating")]        int          Rating,
+    [property: JsonPropertyName("actualityDate")] DateTime     ActualityDate
+);
+
+public record Statistic(
+    [property: JsonPropertyName("category")]      PlayCategory Category,
+    [property: JsonPropertyName("wins")]          int          Wins,
+    [property: JsonPropertyName("losses")]        int          Losses,
+    [property: JsonPropertyName("draws")]         int          Draws,
+    [property: JsonPropertyName("gamesAmount")]   int          GamesAmount,
+    [property: JsonPropertyName("actualityDate")] DateTime     ActualityDate
+);
+
+public record TournamentStatistic(
+    [property: JsonPropertyName("tournament")]          Tournament Tournament,
+    [property: JsonPropertyName("wins")]                int        Wins,
+    [property: JsonPropertyName("losses")]              int        Losses,
+    [property: JsonPropertyName("draws")]               int        Draws,
+    [property: JsonPropertyName("meanOpponentsRating")] double     MeanOpponentsRating,
+    [property: JsonPropertyName("rating")]              int        Rating
+);
+
+public record Tournament(
+    [property: JsonPropertyName("id")]                string   Id,
+    [property: JsonPropertyName("name")]              string   Name,
+    [property: JsonPropertyName("organiserNickname")] string   OrganiserNickname,
+    [property: JsonPropertyName("date")]              DateTime Date
+);
---
+++ src/Backend/DataManagement/LichessApi/TeamResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Backend.DataManagement.LichessApi;
+
+public record TeamResponse(
+    [property: JsonPropertyName("id")]                string                Id,
+    [property: JsonPropertyName("name")]              string                Name,
+    [property: JsonPropertyName("organiserNickname")] string                OrganiserNickname,
+    [property: JsonPropertyName("participants")]      IReadOnlyList<string> Participants,
+    [property: JsonPropertyName("tournaments")]       IReadOnlyList<string> Tournaments
+);
