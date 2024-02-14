bf185d458132125cf260c94177e79b06fdcd639d
aleks <aleks@email.noreply.com>
2024-02-14 23:28:43 +0500

    [Back] Conf: add Serilog config.

--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -1,5 +1,21 @@
+using Serilog;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration)
+                                      .CreateLogger();
+
+builder.Services.AddLogging(static builder =>
+                            {
+                                builder.ClearProviders();
+                                builder.AddSerilog(dispose: true);
+                            });
+
+
+builder.Services.AddHttpLogging(static options =>
+                                {
+                                    options.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.Response;
+                                    options.MediaTypeOptions.AddText("application/json");
+                                });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,4 +31,29 @@ app.UseHttpsRedirection();
 
 app.MapGet("/hello", () => "Hello World!");
 
+app.UseHttpLogging();
+
+Log.Information("I'm alive!");
+
 app.Run();
+
+return;
+
+static void ConfigureAuditSerilog(ISerilogConfigurator configurator)
+{
+    configurator.Message(static @event =>
+                         {
+                             if (@event is not AuditEventHttpClient eventHttpClient)
+                             {
+                                 return @event.ToJson();
+                             }
+
+                             object? contentBody = eventHttpClient.Action?.Response?.Content?.Body;
+                             if (contentBody is string { Length: > 500 } body)
+                             {
+                                 eventHttpClient.Action!.Response!.Content!.Body = body[..500] + "<...>";
+                             }
+
+                             return @event.ToJson();
+                         });
+}
