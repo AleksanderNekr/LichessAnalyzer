7a161bd30087e6187af9260ccc27933324164041
aleks <aleks@email.noreply.com>
2024-02-22 15:26:16 +0500

    [Back] Api (feat): add account delete endpoint.

--- src/Backend/Api/UsersController.cs
+++ src/Backend/Api/UsersController.cs
@@ -7,7 +7,7 @@ namespace Backend.Api;
 
 [ApiController]
 [Route("[controller]")]
-public class UsersController : Controller
+public class UsersController(AuthService authService) : Controller
 {
     [HttpGet("/login")]
     public IActionResult Login()
@@ -20,5 +20,25 @@ public class UsersController : Controller
     public IActionResult Logout()
     {
         return SignOut(new AuthenticationProperties { RedirectUri = "/" }, AuthExtensions.CookieScheme);
+    }
+
+    [HttpDelete("/account")]
+    [Authorize(AuthExtensions.LichessAuthPolicyName)]
+    public async Task<IActionResult> DeleteAccount(CancellationToken cancellationToken)
+    {
+        DeleteUserResult result = await authService.DeleteUserAccountAsync(HttpContext.User, cancellationToken);
+
+        return result switch
+        {
+            DeleteUserResult.NotAuthenticated => Problem("User is not authenticated",
+                                                         statusCode: StatusCodes.Status409Conflict),
+            DeleteUserResult.UserNotInDb => SignOut(new AuthenticationProperties { RedirectUri = "/" },
+                                                    AuthExtensions.CookieScheme),
+            DeleteUserResult.SuccessDelete => SignOut(new AuthenticationProperties { RedirectUri = "/" },
+                                                      AuthExtensions.CookieScheme),
+            DeleteUserResult.Fail => Problem("Cannot delete user account now",
+                                             statusCode: StatusCodes.Status409Conflict),
+            _ => new StatusCodeResult(StatusCodes.Status500InternalServerError)
+        };
     }
 }
