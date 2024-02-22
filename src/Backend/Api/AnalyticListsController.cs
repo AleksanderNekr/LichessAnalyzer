dacf1b453d23e9d1b34191d24c73ec4af944cf49
aleks <aleks@email.noreply.com>
2024-02-22 16:20:22 +0500

    [Back] Api (feat): add AnalyticListsController with create list by players endpoint.

---
+++ src/Backend/Api/AnalyticListsController.cs
@@ -0,0 +1,40 @@
+﻿using Backend.Auth;
+using Backend.DataManagement.Users.Entities;
+using Backend.DataManagement.Users.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Api;
+
+[ApiController]
+[Route("api")]
+public class AnalyticListsController(
+    AuthService           authService,
+    AnalyticsListsService listsService) : Controller
+{
+    [HttpPost("lists")]
+    [Authorize(AuthExtensions.LichessAuthPolicyName)]
+    public async Task<ActionResult<AnalyticsList>> CreateByPlayers(
+        [FromBody] string              name,
+        [FromBody] ICollection<string> playersIds,
+        CancellationToken              cancellationToken)
+    {
+        User? creator = await authService.GetCurrentUserAsync(HttpContext.User, cancellationToken);
+        if (creator is null)
+        {
+            return Forbid();
+        }
+
+        AnalyticsList? list = await listsService.CreateByPlayersAsync(Guid.NewGuid(),
+                                                                      name,
+                                                                      creator,
+                                                                      playersIds,
+                                                                      cancellationToken);
+        if (list is null)
+        {
+            return Conflict($"Max lists limit: {creator.MaxListsCount} reached! Unable to create");
+        }
+
+        return Ok(list);
+    }
+}
