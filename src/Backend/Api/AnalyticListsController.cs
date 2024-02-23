352b47c32348335f459d6d7510aced77be9823a3
aleks <aleks@email.noreply.com>
2024-02-23 15:17:25 +0500

    [Back] Api (feat): add get lists endpoints.

--- src/Backend/Api/AnalyticListsController.cs
+++ src/Backend/Api/AnalyticListsController.cs
@@ -14,6 +14,66 @@ public class AnalyticListsController(
     AnalyticsListsRepository         listsRepository,
     ILogger<AnalyticListsController> logger) : Controller
 {
+    [HttpGet("lists/{id:guid}")]
+    [Authorize(AuthExtensions.LichessAuthPolicyName)]
+    public async Task<ActionResult<AnalyticsList>> GetById(
+        [FromRoute] Guid  id,
+        [FromQuery] bool  includePlayers    = true,
+        CancellationToken cancellationToken = default)
+    {
+        User? owner = await authService.GetCurrentUserAsync(HttpContext.User, cancellationToken);
+        if (owner is null)
+        {
+            logger.LogWarning("Current user is null");
+
+            return Forbid(AuthExtensions.AuthenticationScheme);
+        }
+
+        AnalyticsList? list = includePlayers
+                                  ? await listsRepository.GetByIdWithPlayersAsync(owner, id, cancellationToken)
+                                  : await listsRepository.GetByIdAsync(owner, id, cancellationToken);
+        if (list is null)
+        {
+            logger.LogWarning("Tried to get analytics list with ID: {Id}, but not found", id);
+
+            return NotFound($"Analytics list with ID: {id} not found");
+        }
+
+        Guid userId = await GetCurrentUserIdAsync(cancellationToken);
+
+        if (list.CreatorId == userId)
+        {
+            return Ok(list);
+        }
+
+        logger.LogWarning("Tried to get list ({Id}) not owned by User ({UserId})", id, userId);
+
+        return Forbid(AuthExtensions.AuthenticationScheme);
+    }
+
+    [HttpGet("lists")]
+    [Authorize(AuthExtensions.LichessAuthPolicyName)]
+    public async Task<ActionResult<IEnumerable<AnalyticsList>>> GetAll(
+        [FromQuery] bool  includePlayers    = true,
+        CancellationToken cancellationToken = default)
+    {
+        User? owner = await authService.GetCurrentUserAsync(HttpContext.User, cancellationToken);
+        if (owner is null)
+        {
+            logger.LogWarning("Current user is null");
+
+            return Forbid(AuthExtensions.AuthenticationScheme);
+        }
+
+        IEnumerable<AnalyticsList> lists = includePlayers
+                                               ? listsRepository.GetAllWithPlayers(owner)
+                                                                .AsEnumerable()
+                                               : listsRepository.GetAll(owner)
+                                                                .AsEnumerable();
+
+        return Ok(lists);
+    }
+
     [HttpPost("players-lists")]
     [Authorize(AuthExtensions.LichessAuthPolicyName)]
     public async Task<ActionResult<AnalyticsList>> CreateByPlayers(
@@ -38,5 +98,10 @@ public class AnalyticListsController(
         }
 
         return Ok(list);
+    }
+
+    private async Task<Guid> GetCurrentUserIdAsync(CancellationToken cancellationToken)
+    {
+        return (await authService.GetCurrentUserAsync(HttpContext.User, cancellationToken))!.Id;
     }
 }
