bd2ad26b0e33a591bd8b2fcde0bd27f62c6ea0fb
aleks <aleks@noreply.com>
2024-03-02 16:02:19 +0500

    [Front] Login (feat): add login-menu.component.

---
+++ src/Frontend/src/app/login/login-menu/login-menu.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { AuthService } from "../../auth/auth.service";
+
+@Component({
+  selector: 'app-login-menu',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    RouterLink
+  ],
+  templateUrl: './login-menu.component.html',
+  styleUrl: './login-menu.component.css'
+})
+export class LoginMenuComponent implements OnInit {
+  public isAuthenticated: boolean = false
+  public userName: string | undefined
+
+  constructor(private readonly authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated()
+    this.userName = this.authService.getUserName()
+  }
+
+  logout() {
+    this.authService.logout()
+  }
+}
