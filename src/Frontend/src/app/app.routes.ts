8ae995c3d6db9f10952696702dd84d010d60e74b
aleks <aleks@noreply.com>
2024-03-02 16:03:41 +0500

    [Front] Conf (feat): add routes setup.

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "account/login", component: LoginComponent },
+  { path: "**", component: NotFoundComponent, pathMatch: "full" }
+];
