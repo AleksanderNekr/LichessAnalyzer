7f578ffeef593baa95cbb549f1896227d928dbc7
aleks <aleks@noreply.com>
2024-03-03 18:46:29 +0500

    [Front] Auth (feat): add auth-guard.

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { AccountComponent } from "./account/account.component";
+import { canActivate, canActivateChild } from "./auth/auth-guard";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: "full" },
   { path: "account/login", component: LoginComponent },
-  { path: "account/profile", component: AccountComponent },
+  {
+    path: "account/profile",
+    component: AccountComponent,
+    canActivate: [ canActivate ],
+    canActivateChild: [ canActivateChild ]
+  },
   { path: "**", component: NotFoundComponent, pathMatch: "full" }
 ];
---
+++ src/Frontend/src/app/auth/auth-guard.ts
@@ -0,0 +1,19 @@
+﻿import {
+  ActivatedRouteSnapshot,
+  CanActivateChildFn,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot
+} from "@angular/router";
+import { inject } from "@angular/core";
+import { AuthService } from "./auth.service";
+
+export const canActivate: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.isAuthenticated() ? true : router.createUrlTree([ 'account/login' ])
+};
+
+export const canActivateChild: CanActivateChildFn =
+  (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => canActivate(route, state);
