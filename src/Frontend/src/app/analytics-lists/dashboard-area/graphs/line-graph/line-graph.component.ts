74d32e6fef4f810e71b15369285bd77b3f706354
aleks <aleks@noreply.com>
2024-03-14 12:39:56 +0500

    [Front] Board (refactor): add graph title and title padding as inputs.

--- src/Frontend/src/app/analytics-lists/dashboard-area/graphs/line-graph/line-graph.component.ts
+++ src/Frontend/src/app/analytics-lists/dashboard-area/graphs/line-graph/line-graph.component.ts
@@ -3,51 +3,53 @@ import { NgxEchartsDirective, provideEcharts } from "ngx-echarts";
 import { EChartsOption } from 'echarts'
 
 @Component({
-  selector: 'app-line-graph',
-  standalone: true,
-  imports: [
-    NgxEchartsDirective
-  ],
-  providers: [
-    provideEcharts(),
-  ],
-  template: `
-    <div echarts [options]="options" class="echarts"></div>
-  `
+    selector: 'app-line-graph',
+    standalone: true,
+    imports: [
+        NgxEchartsDirective
+    ],
+    providers: [
+        provideEcharts(),
+    ],
+    template: `
+        <div echarts [options]="options" class="echarts"></div>
+    `
 })
 export class LineGraphComponent implements OnChanges {
-  @Input({ required: true }) xAxisData!: Date[]
-  @Input({ required: true }) yAxisData!: { 'name': string, data: number[], 'type': 'line' }[]
+    @Input({ required: true }) xAxisData!: Date[]
+    @Input({ required: true }) yAxisData!: { 'name': string, data: number[], 'type': 'line' }[]
+    @Input({ required: true }) title!: string
+    @Input({ required: false }) titlePadding: number = 80
 
-  options: EChartsOption = {};
+    options: EChartsOption = {};
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['xAxisData'] || changes['yAxisData']) {
-      this.updateOptions();
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['xAxisData'] || changes['yAxisData']) {
+            this.updateOptions();
+        }
     }
-  }
 
-  private updateOptions(): void {
-    this.options = {
-      xAxis: {
-        type: 'category',
-        data: this.xAxisData.map(date => date.toDateString()),
-      },
-      yAxis: {
-        type: 'value',
-      },
-      series: this.yAxisData,
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b}: {c}',
-      },
-      legend: {
-        name: "Legend",
-        left: 80
-      },
-      title: {
-        text: "Ratings"
-      },
-    };
-  }
+    private updateOptions(): void {
+        this.options = {
+            xAxis: {
+                type: 'category',
+                data: this.xAxisData.map(date => date.toDateString()),
+            },
+            yAxis: {
+                type: 'value',
+            },
+            series: this.yAxisData,
+            tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br/>{b}: {c}',
+            },
+            legend: {
+                name: "Legend",
+                left: this.titlePadding
+            },
+            title: {
+                text: this.title
+            },
+        };
+    }
 }
